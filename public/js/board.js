f467216eff97f31d215c0d30c7e32181c534d508
divinisland <choy0928@kakao.com>
2020-08-14 11:24:24 +0900

    update : check sess.profile.name === item.name

--- public/js/board.js
+++ public/js/board.js
@@ -3,14 +3,17 @@ const target = el("target");
 const updateBtn = el("updateBtn");
 
 const dataList = new Set();
+const authorCheck = new Map();
 
 target.addEventListener("click", (e) => {
   const target = e.target;
 
   if (target.checked) {
     dataList.add(target.dataset.name);
+    authorCheck.set(target.dataset.written, target.dataset.author);
   } else {
     dataList.delete(target.dataset.name);
+    authorCheck.delete(target.dataset.written);
   }
 });
 
@@ -31,16 +34,16 @@ deleteBtn.addEventListener("click", () => {
 });
 
 updateBtn.addEventListener("click", () => {
-  const updateInfo = {
-    list: [...dataList],
-  };
+  const updateList = [...dataList];
+  const contentSeq = updateList[0];
+  const isTrueAuthor = [...authorCheck][0];
 
-  const contentSeq = updateInfo.list[0];
-
-  console.log(contentSeq, "updateValue");
-
-  if (updateInfo.list.length === 1) {
-    window.location.href = `/board/update?contentSeq=${contentSeq}`;
+  if (updateList.length === 1) {
+    if (isTrueAuthor[0] === isTrueAuthor[1]) {
+      window.location.href = `/board/update?contentSeq=${contentSeq}`;
+    } else {
+      alert("본인의 글만 수정 가능합니다.");
+    }
   } else {
     alert("수정은 1개씩 가능합니다.");
   }
